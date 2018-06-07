17d3ffb6e5bcbfde78bb6e177e88139c40417eba
biancavee15 <vasquezbmarie@gmail.com>
2018-06-07 09:53:36 -0400

    added conversations

--- chatbot.py
+++ chatbot.py
@@ -2,15 +2,17 @@ import random
 import time
 import os
 
-name = "Chatty"
-weather = "sunny"
+# name = "Chatty"
+# weather = "sunny"
 greetIn = ["hello", "hi", "sup", "hey"]
 nameIn = ["what is your name", "name"]
 describeIn = ["what are you"]
+favColorIn = ["whats your favorite color", "fav color"]
 
 greetOut = ["hello friend", "hi there"]
 nameOut = ["my name is Chatty", "I'm Chatty"]
-describeOut = ["I'm Chatty a chatty chatbot"]
+describeOut = ["I'm Chatty a chatty chatbot", "a bot obviously"]
+favColorOut = ["green", "pink", "orange", "holo"]
 # responses = {
 #     "What's your name?": [
 #         "my name is {0}".format(name), 
@@ -29,17 +31,23 @@ describeOut = ["I'm Chatty a chatty chatbot"]
     
 #     "default": "My name is {0} Ask me anything you want".format(name)
 # }
-print 'Chatty is listening'
+
+print('Chatty is listening')
+
 while True:
-    message = input('=>')
+    message = raw_input('>>> ')
+    # print(message)
     mesLower = message.lower()
     if message == '':
-        print '>>> See you later!'
+        print('>>> See you later!')
         time.sleep(1)
-        os.system("sudo shutdown -h now")
     elif mesLower in nameIn:
-            print '>>>' + (random.choice(nameOut))
+        print '>>>' + (random.choice(nameOut))
     elif mesLower in greetIn: 
-            print '>>>' + (random.choice(greetOut))
+        print '>>>' + (random.choice(greetOut))
     elif mesLower in describeIn:
-            print '>>>' + (describeOut)
+        print '>>>' + (random.choice(describeOut))
+    elif mesLower in favColorIn:
+        print '>>>' + (random.choice(favColorOut))
+    else:
+        print ("Sorry, I don't understand") 
